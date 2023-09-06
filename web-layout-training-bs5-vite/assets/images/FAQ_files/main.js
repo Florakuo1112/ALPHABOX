c9e83bb2c082263ffef56a32564b3462e6a028fb
FloraKuo <tingyu1112@gmail.com>
2023-09-07 00:34:54 +0800

    0907updated

---
+++ web-layout-training-bs5-vite/assets/images/FAQ_files/main.js
@@ -0,0 +1,4 @@
+import '/ALPHABOX/assets/scss/all.scss?t=1694012220936';
+import "/ALPHABOX/node_modules/.vite/deps/bootstrap_dist_js_bootstrap__min__js.js?v=6019f44e";
+
+console.log("Hello world!");
